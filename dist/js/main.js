12c71734bf92b661741198bf9c43f923c2a1c6db
Phillip A. Wessels <phillip.a.wessels@gmail.com>
2020-10-21 23:25:02 -0700

    Finished stuff, still a few items to do
    • Prevent propagation (bubbling up) when a tag is removed
    • Change toggle to radio buttons so it passes the freeCodeCamp test? not sure it's possible but we'll see
    • Create animation for submit button & script the class change on click
    • Exclude stuff not preferred

--- dist/js/main.js
+++ dist/js/main.js
@@ -183,14 +183,117 @@ Object.keys(addTagButtons).forEach(function (addTagButton) {
     });
 
 
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 
 
+/////////////////////// CHECKLIST - LISTEN FOR CHECKBOX CHANGE //////////////////////////
+// Array of checklists
+const checklists = document.getElementsByClassName('checkbox-box');
 
+// Function to pass in below loops - listens for clicks & decides what to do
+function checklistInputChange(input) {
+  input.addEventListener('click', function () {
+    let checkboxValue = input.parentElement.getElementsByClassName('checkbox-box__text')[0].textContent;
+    let checkboxTagListID = input.parentElement.parentElement.parentElement.getElementsByClassName('checkbox-box__summary')[0].getElementsByClassName('checkbox-box__tags')[0].id;
+
+    if (input.checked) {
+      console.log('Checked ' + input.id)
+      // If checked, need to add tag via addCheckboxTag()
+      addCheckboxTag(checkboxValue, checkboxTagListID);
+    } else {
+      console.log('Unchecked ' + input.id)
+      // If unchecked, need to remove tag via removeCheckboxTag()     
+      removeCheckboxTag(checkboxValue, checkboxTagListID);
+    }
+  });
+}
+
+// Loop through checklists & loop through inputs
+Object.keys(checklists).forEach(function (checklist) {
+  let checklistInputs = checklists[checklist].getElementsByClassName('checkbox-box__checkbox');
+  
+  // Loop through inputs & call checklistInputChange()
+  Object.keys(checklistInputs).forEach(function (input) {
+    checklistInputChange(checklistInputs[input]);
+  });
+});
+
+/////////////////////// ADD CHECKBOX TAG //////////////////////////
+
+function addCheckboxTag(checkboxValue, checkboxTagListID) {
+  console.log("Checkbox tag list:");
+  let checkboxTagList = document.getElementById(checkboxTagListID);
+  console.log(checkboxTagList);
+
+  // Checkbox tag list name
+  let checkboxTagListName = checkboxTagList.parentElement.childNodes[0].nodeValue;
+  
+  // Create checkbox tag ID
+  let checkboxTagID = checkboxTagListID + '-' + checkboxValue.replace(/\s/g, '') + '-tag';
+
+  // Create checkbox tag element & text node, set attributes  
+  let checkboxTag = document.createElement('li');
+    checkboxTag.setAttribute('class', 'checkbox-box__tag');
+    checkboxTag.setAttribute('id', checkboxTagID);
+
+  let checkboxTagRemove = document.createElement('a');
+    checkboxTagRemove.setAttribute('class','checkbox-box__tag-remove');
+    checkboxTagRemove.setAttribute('title','Uncheck "' + checkboxValue + '" from ' + checkboxTagListName);
+  let checkboxTagRemoveIcon = document.createElement('i');
+    checkboxTagRemoveIcon.setAttribute('class', 'fas fa-times');
+    
+  let checkboxTagText = document.createTextNode(checkboxValue);
+    
+  // Append children to complete total tag element for checkbox
+  checkboxTagRemove.appendChild(checkboxTagRemoveIcon);
+  checkboxTag.appendChild(checkboxTagRemove);
+  checkboxTag.appendChild(checkboxTagText);
+
+  checkboxTagList.appendChild(checkboxTag);
+
+  console.log('Added ' + checkboxValue + ' tag:');
+  console.log(checkboxTag);
+
+  // Tag remove click
+  checkboxTagRemove.addEventListener('click', () => {
+    // Remove tag
+    removeCheckboxTag(checkboxValue, checkboxTagListID);
+
+    // Create checkbox input ID from value
+    let checkboxID = checkboxValue.replace(/\s/g, '');
+    // Uncheck item
+    document.getElementById(checkboxID).checked = false;
+  });
+
+  // TODO: Prevent propagation
+}
+
+/////////////////////// REMOVE CHECKBOX TAG //////////////////////////
+function removeCheckboxTag(checkboxValue, checkboxTagListID) {
+  console.log('Removed ' + checkboxValue + ':');
+  let checkboxTagList = document.getElementById(checkboxTagListID);
+  console.log(checkboxTagList);
+
+  let checkboxTagID = checkboxTagListID + '-' + checkboxValue.replace(/\s/g, '') + '-tag';
+  console.log(checkboxTagID);
+
+  let checkboxTag = document.getElementById(checkboxTagID);
+  console.log(checkboxTag);
+  
+  checkboxTagList.removeChild(checkboxTag);
+}
 
 
 /////////////////////// SUBMIT CHECKBOX //////////////////////////
 
+// Loop through checkbox inputs & call addCheckboxPressEnter()
 const addCheckboxInputs = document.getElementsByClassName('checkbox-box__add-input');
+Object.keys(addCheckboxInputs).forEach(function (addCheckboxInput) {
+  addCheckboxPressEnter(addCheckboxInputs[addCheckboxInput]);
+});
+
+// Function to pass checkboxList/checkboxInput to addCheckbox() when pressing enter
 function addCheckboxPressEnter(addCheckboxInput) {
   addCheckboxInput.addEventListener("keyup", function(event) {
     if (event.code === 'Enter') {
@@ -203,13 +306,13 @@ function addCheckboxPressEnter(addCheckboxInput) {
   });
 }
 
-// Loop through checkbox inputs & call addCheckboxPressEnter()
-  Object.keys(addCheckboxInputs).forEach(function (addCheckboxInput) {
-    addCheckboxPressEnter(addCheckboxInputs[addCheckboxInput]);
-  });
+// Loop through add checkbox buttons & call addCheckboxButtonClick()
+const addCheckboxButtons = document.getElementsByClassName('checkbox-box__add-btn');
+Object.keys(addCheckboxButtons).forEach(function (addCheckboxButton) {
+  addCheckboxButtonClick(addCheckboxButtons[addCheckboxButton]);
+});
 
 // Function to pass checkboxList/checkboxInput to addCheckbox() when clicking add button
-const addCheckboxButtons = document.getElementsByClassName('checkbox-box__add-btn');
 function addCheckboxButtonClick(addCheckboxButton) {
   addCheckboxButton.addEventListener("click", function() {
     let checkboxTagListID = this.parentNode.parentNode.getElementsByClassName('checkbox-box__summary')[0].getElementsByClassName('checkbox-box__tags')[0].id;
@@ -221,16 +324,11 @@ function addCheckboxButtonClick(addCheckboxButton) {
   });
 }
 
-// Loop through add checkbox buttons & call addCheckboxButtonClick()
-  Object.keys(addCheckboxButtons).forEach(function (addCheckboxButton) {
-    addCheckboxButtonClick(addCheckboxButtons[addCheckboxButton]);
-  });
-
-/////////////////////// ADD CHECKLIST ITEM //////////////////////////
+/////////////////////// ADD CUSTOM CHECKLIST ITEM //////////////////////////
 
 function addCheckbox(checkboxValue, checkboxTagListID) {
   let checkList = document.getElementById(checkboxTagListID).parentNode.parentNode.getElementsByClassName('checkbox-box__expand-container')[0];
-
+  console.log('Checking if checkbox exists...');
   console.log(checkList.querySelector('[value="' + checkboxValue + '"'));
 
   if (checkboxValue == "" || checkboxValue == null || checkboxValue == 'Add from set...') {
@@ -239,45 +337,15 @@ function addCheckbox(checkboxValue, checkboxTagListID) {
     console.log('Checkbox exists')
   } else {
     console.log("Function received " + checkboxValue);
-    console.log("Checkbox tag list:");
-    let checkboxTagList = document.getElementById(checkboxTagListID);
-    console.log(checkboxTagList);
+    
+    // Add tag
+    addCheckboxTag(checkboxValue, checkboxTagListID);
+
+    // Add to checklist
     console.log("Checklist:")
     console.log(checkList);
 
-    // Checkbox tag list name
-    let checkboxTagListName = checkboxTagList.parentElement.childNodes[0].nodeValue;
-
-    // Create checkbox tag element & text node, set attributes  
-    let checkboxTag = document.createElement('li');
-      checkboxTag.setAttribute('class', 'checkbox-box__tag');
-  
-    let checkboxTagRemove = document.createElement('a');
-      checkboxTagRemove.setAttribute('class','checkbox-box__tag-remove');
-      checkboxTagRemove.setAttribute('title','Uncheck "' + checkboxValue + '" from ' + checkboxTagListName);
-    let checkboxTagRemoveIcon = document.createElement('i');
-      checkboxTagRemoveIcon.setAttribute('class', 'fas fa-check-double');
-      
-    let checkboxTagText = document.createTextNode(checkboxValue);
-      
-    // Append children to complete total tag element for checkbox
-    checkboxTagRemove.appendChild(checkboxTagRemoveIcon);
-    checkboxTag.appendChild(checkboxTagRemove);
-    checkboxTag.appendChild(checkboxTagText);
-  
-    checkboxTagList.appendChild(checkboxTag);
-
-    console.log('Added ' + checkboxValue + ' tag:');
-    console.log(checkboxTag);
-
-    // Tag remove click
-    checkboxTagRemove.addEventListener('click', () => {
-      console.log('Removed ' + checkboxValue + ':');
-      console.log(checkboxTag);
-      checkboxTag.parentElement.removeChild(checkboxTag);
-    });
-
-    // Get the value from the input (I didn't assign an ID because it's create dynamically)
+    // Create checkbox input ID from value
     let checkboxID = checkboxValue.replace(/\s/g, '');
       
     // Create added checkbox elements & text node, set attributes
@@ -303,9 +371,8 @@ function addCheckbox(checkboxValue, checkboxTagListID) {
     addedCheckboxLabel.appendChild(addedCheckboxInput);
     addedCheckboxLabel.appendChild(addedCheckboxSpan);
 
-    ///////////////// TODO: ADD CHECKBOX TO LIST !!!! ///////////////
+    // Add checkbox element to checklist
     checkList.insertBefore(addedCheckboxLabel, checkList.getElementsByClassName('checkbox-box__add-input')[0]);
-
       
     // add click listeners to add animation class
     addedCheckboxInput.addEventListener("click", function () {
@@ -322,134 +389,12 @@ function addCheckbox(checkboxValue, checkboxTagListID) {
       );
     }, false);
 
+    // Add click listener for state change
+    checklistInputChange(addedCheckboxInput);
+
+    // Completion Log
     console.log('Added ' + checkboxValue + ' checkbox:');
     console.log(addedCheckboxLabel);
         
   }
 }
-
-
-
-
-
-
-
-
-
-
-// Function to add gender
-let addOtherGenderButton = document.getElementById('addOtherGenderButton');
-let genderList = document.getElementById('genderList');
-
-addOtherGenderButton.addEventListener('click', function () {
-  // Create add gender box elements & text node, set attributes
-  const addGenderBox = document.createElement('div');
-  
-  const addGenderCancel = document.createElement('a');
-    addGenderCancel.setAttribute('class', 'cancel-add-gender');
-    addGenderCancel.setAttribute('title', 'Cancel');
-  const addGenderCancelIcon = document.createElement('i');
-    addGenderCancelIcon.setAttribute('class', 'fas fa-times');
-  
-  let addGenderInput = document.createElement('input');
-    addGenderInput.setAttribute('type', 'text');
-    addGenderInput.setAttribute('placeholder', 'Your gender');
-    addGenderInput.setAttribute('class', 'form__textinput-input addGenderButton');
-    
-  const addGenderAddBtn = document.createElement('button');
-    addGenderAddBtn.setAttribute('type', 'button');
-    addGenderAddBtn.setAttribute('class', 'btn--add');
-    addGenderAddBtn.setAttribute('title', 'Add Gender');
-    
-  const addGenderAddIcon = document.createElement('i');
-    addGenderAddIcon.setAttribute('class', 'fas fa-plus');
-    
-  // Append children to complete total element for add gender box
-  addGenderCancel.appendChild(addGenderCancelIcon);
-  addGenderAddBtn.appendChild(addGenderAddIcon);
-  addGenderBox.appendChild(addGenderCancel);
-  addGenderBox.appendChild(addGenderInput);
-  addGenderBox.appendChild(addGenderAddBtn);
-
-  // Add gender box replaces other button (this)
-  genderList.replaceChild(addGenderBox, this);
-  // Focus on input (I didn't assign an ID because it's create dynamically)
-  addGenderInput.focus();
-  
-  // Cancel button - other button (this) replaces add gender box
-  addGenderCancel.addEventListener('click', () => {
-    genderList.replaceChild(this, addGenderBox);
-  });
-
-  // Function to create gender element
-  const createGender = () => {
-    // Check if field is empty - if so nothing happens
-    if (addGenderInput.value == "" || addGenderInput.value == null) {
-      console.log('Gender input is empty');
-    } else {
-      // Get the value from the input (I didn't assign an ID because it's create dynamically)
-      const genderValue = addGenderInput.value
-      const genderID = genderValue.replace(/\s/g, '');
-  
-      // Create added gender elements & text node, set attributes
-      const addedGender = document.createElement('label');
-        addedGender.setAttribute('for', genderID);
-        addedGender.setAttribute('class', 'checkbox-box__checkbox-label');
-  
-      const addedGenderCheckbox = document.createElement('input');
-        addedGenderCheckbox.setAttribute('type', 'checkbox');
-        addedGenderCheckbox.setAttribute('id', genderID);
-        addedGenderCheckbox.setAttribute('class', 'checkbox checkbox-box__checkbox');
-        addedGenderCheckbox.setAttribute('name', 'gender');
-        addedGenderCheckbox.setAttribute('value', genderValue);
-        addedGenderCheckbox.setAttribute('checked', '');
-  
-      const addedGenderSpan = document.createElement('span');
-        addedGenderSpan.setAttribute('class', 'checkbox-box__text');
-  
-      const addedGenderText = document.createTextNode("\n" + genderValue);
-  
-      // Append children to complete total element for added gender
-      addedGenderSpan.appendChild(addedGenderText);
-      addedGender.appendChild(addedGenderCheckbox);
-      addedGender.appendChild(addedGenderSpan);
-  
-      // Update gender list
-      // Insert new gender element before this, while add other button replaces this
-      genderList.insertBefore(addedGender, addGenderBox);
-      genderList.replaceChild(this, addGenderBox);
-  
-      // add click listeners to add animation class
-      addedGenderCheckbox.addEventListener("click", function () {
-        addedGenderCheckbox.preventDefault;
-        addedGenderCheckbox.classList.remove("activated");
-        void addedGenderCheckbox.offsetWidth;
-        addedGenderCheckbox.classList.add("activated");
-        setTimeout(
-          function (checkbox) {
-            checkbox.classList.remove("activated");
-          },
-          parseFloat(getComputedStyle(addedGenderCheckbox).animationDuration, 10) * 1000,
-          addedGenderCheckbox
-        );
-      }, false);
-
-      console.log('Added ' + genderValue + ':');
-      console.log(addedGender);
-    }
-  }
-
-  // Creating & adding the gender element after clicking add button
-  addGenderAddBtn.addEventListener('click', () => {
-    console.log('Clicked add button to add gender;');
-    createGender();
-  });
-
-  // Creating & adding the gender element after pressing enter
-  addGenderInput.addEventListener("keyup", function(event) {
-    if (event.code === 'Enter') {
-      console.log('Enter pressed in add gender input;');
-      createGender();
-    }
-  });
-});
